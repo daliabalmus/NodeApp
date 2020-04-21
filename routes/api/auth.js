171ccf12ded77cd0fc168cbdc9ca60fae376e4c6
Dalia Balmus <30559125+daliabalmus@users.noreply.github.com>
2020-04-21 11:13:18 +0300

    token expiration longer lasting

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -56,7 +56,7 @@ router.post('/', [
                                 }
                         };
                         jwt.sign(payload, config.get("jwtSecret"),
-                                {expiresIn: 360000},
+                                {expiresIn: 3600000000},
                                 (err,token) => {
                                         if (err) throw err;
                                         res.json({ token });
