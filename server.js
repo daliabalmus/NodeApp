074cc3ac5e393f11d70d915f11f7f8142f171192
Dalia Balmus <30559125+daliabalmus@users.noreply.github.com>
2020-05-08 15:10:30 +0300

    server setting

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 
 // Set up mongoose connection
-
+const path = require('path')
 const express = require('express');
 const connectDB = require('./config/db');
 // const usersRoute = );
@@ -11,7 +11,7 @@ const app = express();
 connectDB();
 
 
-app.get('/', (req, res) => res.send('API RUNNING'));
+// app.get('/', (req, res) => res.send('API RUNNING'));
 
 // Init middleware
 app.use(express.json({extended: false}));
@@ -21,6 +21,15 @@ app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/my-profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
+
+// serve static assets in production
+if(process.env.NODE_ENV === 'production') {
+       // set static folder
+       app.use(express.static('client/build'));
+       app.get("*", (req, res) => {
+            res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+       });
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
